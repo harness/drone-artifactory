f31de402ab7ff7c41b16263d5248d20bf5406211
Shubham Agrawal <shubham.agrawal@harness.io>
2020-11-04 15:33:00 +0530

    Upload with basic params on amd64

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -4,7 +4,12 @@
 
 package plugin
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+)
 
 // Args provides plugin execution arguments.
 type Args struct {
@@ -14,12 +19,51 @@ type Args struct {
 	Level string `envconfig:"PLUGIN_LOG_LEVEL"`
 
 	// TODO replace or remove
-	Param1 string `envconfig:"PLUGIN_PARAM1"`
-	Param2 string `envconfig:"PLUGIN_PARAM2"`
+	Username       string `envconfig:"PLUGIN_USERNAME"`
+	Password       string `envconfig:"PLUGIN_PASSWORD"`
+	APIKey         string `envconfig:"PLUGIN_API_KEY"`
+	AccessToken    string `envconfig:"PLUGIN_ACCESS_TOKEN"`
+	URL            string `envconfig:"PLUGIN_URL"`
+	SrcFilePattern string `envconfig:"PLUGIN_SRC_FILE"`
+	TargetPattern  string `envconfig:"PLUGIN_TARGET_FILE"`
+	Retries        int    `envconfig:"PLUGIN_RETRIES"`
 }
 
 // Exec executes the plugin.
 func Exec(ctx context.Context, args Args) error {
 	// write code here
-	return nil
+	if args.URL == "" {
+		return fmt.Errorf("url needs to be set")
+	}
+
+	cmdArgs := []string{"rt", "u", fmt.Sprintf("--url=%s", args.URL)}
+	if args.Retries != 0 {
+		cmdArgs = append(cmdArgs, fmt.Sprintf("--retries=%d", args.Retries))
+	}
+
+	// Set authentication params
+	if args.Username != "" && args.Password != "" {
+		cmdArgs = append(cmdArgs, fmt.Sprintf("--user=%s", args.Username))
+		cmdArgs = append(cmdArgs, fmt.Sprintf("--password=%s", args.Password))
+	} else if args.APIKey != "" {
+		cmdArgs = append(cmdArgs, fmt.Sprintf("--apikey=%s", args.APIKey))
+	} else if args.AccessToken != "" {
+		cmdArgs = append(cmdArgs, fmt.Sprintf("--access-token=%s", args.AccessToken))
+	} else {
+		return fmt.Errorf("either username/password, api key or access token needs to be set")
+	}
+
+	if args.SrcFilePattern == "" {
+		return fmt.Errorf("source file needs to be set")
+	}
+	if args.TargetPattern == "" {
+		return fmt.Errorf("target file needs to be set")
+	}
+	cmdArgs = append(cmdArgs, args.SrcFilePattern, args.TargetPattern)
+
+	cmd := exec.Command("jfrog", cmdArgs...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	return err
 }
