dc3a21dc3a8499e1da51b3402dd5f1824d26841b
Shubham Agrawal <shubham.agrawal@harness.io>
2022-02-04 16:48:35 +0530

    Use smaller windows base image (#12)

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -80,7 +80,7 @@ func Exec(ctx context.Context, args Args) error {
 
 func getShell() (string, string) {
 	if runtime.GOOS == "windows" {
-		return "powershell", "-Command"
+		return "pwsh", "-Command"
 	}
 
 	return "sh", "-c"
