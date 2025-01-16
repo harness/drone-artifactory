0436774f35651d58b2df710137c17bc8ebae30ff
c_senthil.kumar@harness.io <c_senthil.kumar@harness.io>
2025-01-16 20:41:47 +0530

    added upload from direct spec

--- plugin/rt_upload_download.go
+++ plugin/rt_upload_download.go
@@ -2,6 +2,8 @@ package plugin
 
 import (
 	"fmt"
+	"os"
+	"time"
 )
 
 // rtBuildInfo
@@ -52,6 +54,16 @@ func GetUploadCommandArgs(args Args) ([][]string, error) {
 		return cmdList, err
 	}
 
+	if args.Spec != "" {
+		fileName := getTimestampedFileName()
+		err = writeToFile(fileName, args.Spec)
+		if err != nil {
+			return cmdList, err
+		}
+		args.Spec = ""
+		args.SpecPath = fileName
+	}
+
 	uploadCommandArgs := []string{"rt", "upload", args.Source, args.Target}
 	err = PopulateArgs(&uploadCommandArgs, &args, UploadCmdJsonTagToExeFlagMapStringItemList)
 	if err != nil {
@@ -66,6 +78,27 @@ func GetUploadCommandArgs(args Args) ([][]string, error) {
 	}
 
 	return cmdList, nil
+}
+
+func writeToFile(filePath, content string) error {
+	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to open file: %v", err)
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(content)
+	if err != nil {
+		return fmt.Errorf("failed to write to file: %v", err)
+	}
+
+	return nil
+}
+
+func getTimestampedFileName() string {
+	timestamp := time.Now().Format("20060102_150405.000")
+	fileName := fmt.Sprintf("%s_spec.json", timestamp)
+	return fileName
 }
 
 var DownloadCmdJsonTagToExeFlagMapStringItemList = []JsonTagToExeFlagMapStringItem{
