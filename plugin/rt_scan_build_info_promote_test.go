95cfc0357e549cf7c00e3dcb0977e20f98d945a1
c_senthil.kumar@harness.io <c_senthil.kumar@harness.io>
2025-01-27 20:03:46 +0530

    adding unit test case for rt promote

--- plugin/rt_scan_build_info_promote_test.go
+++ plugin/rt_scan_build_info_promote_test.go
@@ -91,62 +91,32 @@ func TestGetBuildInfoPublishCommandUserPassword(t *testing.T) {
 	}
 }
 
-//
-//func TestGetDownloadCommandUserAccessToken(t *testing.T) {
-//	args := Args{
-//		AccessToken: RtAccessToken,
-//		Command:     "download",
-//		BuildName:   RtBuildName,
-//		BuildNumber: RtBuildNumber,
-//		URL:         RtUrlTestStr,
-//		Module:      RtModule,
-//		Project:     RtProject,
-//		SpecPath:    "spec.json",
-//	}
-//	cmdList, err := GetDownloadCommandArgs(args)
-//	if err != nil {
-//		t.Errorf("Unexpected error: %v", err)
-//	}
-//
-//	wantCmds := []string{
-//		"config add tmpServerId --url=https://artifactory.test.io/artifactory/ " +
-//			"--access-token $PLUGIN_ACCESS_TOKEN --interactive=false",
-//		"rt download   --build-name=t2 --build-number=v1.0 --module=backend_module " +
-//			"--project=backend_project --spec=spec.json",
-//	}
-//
-//	for i, cmd := range cmdList {
-//		cmdStr := strings.Join(cmd, " ")
-//		if !strings.Contains(cmdStr, wantCmds[i]) {
-//			t.Errorf("Expected: |%s|, Got: |%s|", wantCmds[i], cmdStr)
-//		}
-//	}
-//}
-//
-//func TestGetCleanupCommandUserPassword(t *testing.T) {
-//	args := Args{
-//		Username:    "ab",
-//		Password:    "cd",
-//		Command:     "cleanup",
-//		BuildName:   RtBuildName,
-//		BuildNumber: RtBuildNumber,
-//		URL:         RtUrlTestStr,
-//	}
-//	cmdList, err := GetCleanupCommandArgs(args)
-//	if err != nil {
-//		t.Errorf("Unexpected error: %v", err)
-//	}
-//
-//	wantCmds := []string{
-//		"config add tmpServerId --url=https://artifactory.test.io/artifactory/ " +
-//			"--user $PLUGIN_USERNAME --password $PLUGIN_PASSWORD --interactive=false",
-//		"rt build-clean t2 v1.0",
-//	}
-//
-//	for i, cmd := range cmdList {
-//		cmdStr := strings.Join(cmd, " ")
-//		if !strings.Contains(cmdStr, wantCmds[i]) {
-//			t.Errorf("Expected: |%s|, Got: |%s|", wantCmds[i], cmdStr)
-//		}
-//	}
-//}
+func TestPromoteBuildCommandUserPassword(t *testing.T) {
+	args := Args{
+		Username:    "ab",
+		Password:    "cd",
+		Command:     "promote",
+		BuildName:   RtBuildName,
+		BuildNumber: RtBuildNumber,
+		URL:         RtUrlTestStr,
+		Target:      "promoted-repo",
+		Copy:        "true",
+	}
+	cmdList, err := GetPromoteCommandArgs(args)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	wantCmds := []string{
+		"config add tmpServeId --url=https://artifactory.test.io/artifactory/ " +
+			"--user $PLUGIN_USERNAME --password $PLUGIN_PASSWORD --interactive=false",
+		"rt build-promote --copy=true t2 v1.0 promoted-repo",
+	}
+
+	for i, cmd := range cmdList {
+		cmdStr := strings.Join(cmd, " ")
+		if !strings.Contains(cmdStr, wantCmds[i]) {
+			t.Errorf("Expected: |%s|, Got: |%s|", wantCmds[i], cmdStr)
+		}
+	}
+}
