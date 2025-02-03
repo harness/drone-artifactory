5e17e1569c896e32dad833005c98c385857e2394
c_senthil.kumar@harness.io <c_senthil.kumar@harness.io>
2025-02-03 19:57:59 +0530

    supporting build-add-dependencies

--- plugin/rt_scan_build_info_promote.go
+++ plugin/rt_scan_build_info_promote.go
@@ -64,3 +64,49 @@ func GetPromoteCommandArgs(args Args) ([][]string, error) {
 	cmdList = append(cmdList, promoteCommandArgs)
 	return cmdList, nil
 }
+
+var AddDependenciesCmdJsonToExeFlagMapItemList = []JsonTagToExeFlagMapStringItem{
+	{"--exclusions=", "PLUGIN_EXCLUSIONS", false, false},
+	{"--from-rt=", "PLUGIN_FROM_RT", false, false},
+	{"--module=", "PLUGIN_MODULE", false, false},
+	{"--project=", "PLUGIN_PROJECT", false, false},
+	{"--recursive=", "PLUGIN_RECURSIVE", false, false},
+	{"--regexp=", "PLUGIN_REGEXP", false, false},
+	{"--server-id=", "PLUGIN_SERVER_ID", false, false},
+	{"--spec=", "PLUGIN_SPEC_PATH", false, false},
+	{"--spec-vars=", "PLUGIN_SPEC_VARS", false, false},
+}
+
+func GetAddDependenciesCommandArgs(args Args) ([][]string, error) {
+	var cmdList [][]string
+
+	jfrogConfigAddConfigCommandArgs, err := GetConfigAddConfigCommandArgs(tmpServerId,
+		args.Username, args.Password, args.URL, args.AccessToken, args.APIKey)
+	if err != nil {
+		return cmdList, err
+	}
+
+	addDependenciesCommandArgs := []string{"rt", "build-add-dependencies"}
+	err = PopulateArgs(&addDependenciesCommandArgs, &args, AddDependenciesCmdJsonToExeFlagMapItemList)
+	if err != nil {
+		return cmdList, err
+	}
+	addDependenciesCommandArgs = append(addDependenciesCommandArgs, "--server-id="+tmpServerId)
+
+	addDependenciesCommandArgs = append(addDependenciesCommandArgs, args.BuildName, args.BuildNumber)
+	if args.DependencyPattern != "" {
+		addDependenciesCommandArgs = append(addDependenciesCommandArgs, args.DependencyPattern)
+	}
+
+	buildInfoCommandArgs := []string{"rt", "build-publish", args.BuildName, args.BuildNumber}
+	err = PopulateArgs(&buildInfoCommandArgs, &args, nil)
+	if err != nil {
+		return cmdList, err
+	}
+
+	cmdList = append(cmdList, jfrogConfigAddConfigCommandArgs)
+	cmdList = append(cmdList, addDependenciesCommandArgs)
+	cmdList = append(cmdList, buildInfoCommandArgs)
+
+	return cmdList, nil
+}
